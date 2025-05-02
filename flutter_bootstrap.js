b0eb8d77618881b0134cbb4bf5c46398971cf002
coiler <coiler1@jh.edu>
2025-05-02 14:09:43 -0400

    adding another UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1489208244"
+    serviceWorkerVersion: "2495260396"
   }
 });
